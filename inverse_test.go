37c9fdfc77c6382755f2091305f08885d18020cf
Akito Ito <akito0107@gmail.com>
2019-05-19 23:54:09 +0900

    feat: add inverse test

---
+++ inverse_test.go
@@ -0,0 +1,149 @@
+package xmigrate
+
+import (
+	"testing"
+
+	"github.com/akito0107/xsqlparser/sqlast"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestInverse(t *testing.T) {
+
+	cases := []struct {
+		name    string
+		current string
+		diff    *SchemaDiff
+		expect  *SchemaDiff
+	}{
+		{
+			name:    "add table",
+			current: "create table test1(id int primary key);",
+			diff: &SchemaDiff{
+				Type: AddTable,
+				Spec: &AddTableSpec{
+					SQL: &sqlast.SQLCreateTable{
+						Name: sqlast.NewSQLObjectName("test2"),
+						Elements: []sqlast.TableElement{
+							&sqlast.SQLColumnDef{
+								Name:     sqlast.NewSQLIdent("id"),
+								DataType: &sqlast.Int{},
+								Constraints: []*sqlast.ColumnConstraint{
+									{
+										Spec: &sqlast.UniqueColumnSpec{
+											IsPrimaryKey: true,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expect: &SchemaDiff{
+				Type: DropTable,
+				Spec: &DropTableSpec{
+					TableName: "test2",
+				},
+			},
+		},
+		{
+			name: "drop table",
+			current: `create table test1(id int primary key);
+create table test2(id int primary key);`,
+			diff: &SchemaDiff{
+				Type: DropTable,
+				Spec: &DropTableSpec{
+					TableName: "test2",
+				},
+			},
+			expect: &SchemaDiff{
+				Type: AddTable,
+				Spec: &AddTableSpec{
+					SQL: &sqlast.SQLCreateTable{
+						Name: sqlast.NewSQLObjectName("test2"),
+						Elements: []sqlast.TableElement{
+							&sqlast.SQLColumnDef{
+								Name:     sqlast.NewSQLIdent("id"),
+								DataType: &sqlast.Int{},
+								Constraints: []*sqlast.ColumnConstraint{
+									{
+										Spec: &sqlast.UniqueColumnSpec{
+											IsPrimaryKey: true,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:    "add column",
+			current: `create table test1(id int primary key);`,
+			diff: &SchemaDiff{
+				Type: AddColumn,
+				Spec: &AddColumnSpec{
+					TableName: "test1",
+					ColumnDef: &sqlast.SQLColumnDef{
+						Name:     sqlast.NewSQLIdent("name"),
+						DataType: &sqlast.VarcharType{},
+						Constraints: []*sqlast.ColumnConstraint{
+							{
+								Spec: &sqlast.NotNullColumnSpec{},
+							},
+						},
+					},
+				},
+			},
+			expect: &SchemaDiff{
+				Type: DropColumn,
+				Spec: &DropColumnSpec{
+					TableName:  "test1",
+					ColumnName: "name",
+				},
+			},
+		},
+		{
+			name:    "drop column",
+			current: `create table test1(id int primary key, name varchar not null);`,
+			diff: &SchemaDiff{
+				Type: DropColumn,
+				Spec: &DropColumnSpec{
+					TableName:  "test1",
+					ColumnName: "name",
+				},
+			},
+			expect: &SchemaDiff{
+				Type: AddColumn,
+				Spec: &AddColumnSpec{
+					TableName: "test1",
+					ColumnDef: &sqlast.SQLColumnDef{
+						Name:     sqlast.NewSQLIdent("name"),
+						DataType: &sqlast.VarcharType{},
+						Constraints: []*sqlast.ColumnConstraint{
+							{
+								Spec: &sqlast.NotNullColumnSpec{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			def := genTableDef(t, c.current)
+			act, err := Inverse(c.diff, def)
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+
+			if d := cmp.Diff(act, c.expect, IgnoreMarker); d != "" {
+				t.Errorf("diff: %s", d)
+			}
+		})
+	}
+
+}
